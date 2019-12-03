dbccadf5ffabc376afb5461a8aa0c6e8b8d5549b
swedi <swedi@LAPTOP-UD9JK3H7>
2019-12-03 10:53:17 -0600

    Added TODO message to BarcodeTester.java

--- AP_CompSci/src/chapter8/BarcodeTester.java
+++ AP_CompSci/src/chapter8/BarcodeTester.java
@@ -7,7 +7,7 @@ public class BarcodeTester {
 		
 		Scanner in = new Scanner(System.in);
 		int [] zipcode = new int [5];
-		String [] 
+		String [] barcode = new String [5];
 		
 		System.out.println("Enter barcode");
 		String nums = in.next();
@@ -21,8 +21,8 @@ public class BarcodeTester {
 		System.out.println("Enter barcode to be translated into numbers: ");
 		nums = in.next();
 		for (int i = 0; i<5; i++) {
-			barcode[i] = nums.charAt(i)-48;
+			barcode[i] = nums.substring(1+(i*5), 6+(i*5));
 		}
-		
+		//TODO finish this
 	}
 }

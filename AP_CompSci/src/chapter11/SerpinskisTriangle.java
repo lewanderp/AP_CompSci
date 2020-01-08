945cda3b95531c900f3883f70be882412d53aab3
Philip Lewander <swedishassassin1@outlook.com>
2020-01-08 11:26:57 -0600

    Completed serpisnskis triangle

---
+++ AP_CompSci/src/chapter11/SerpinskisTriangle.java
@@ -0,0 +1,47 @@
+package chapter11;
+
+import java.awt.Color;
+import resources.Canvas;
+
+public class SerpinskisTriangle {
+    public static Canvas c = new Canvas("Serpinski's Triangle");
+    public static void main (String [] args) throws InterruptedException {
+        c.setSize(1000, 500);
+        c.setVisible(true);
+        drawBackground();
+        for(int i = 0; i < 5; i++){
+            drawBackground();
+            c.setInkColor(Color.cyan);
+            serpinskisTriangle(i, 400, 300, 500, 100);
+            c.setInkColor(Color.white);
+            c.setFontSize(20);
+            c.drawString(""+i+1, 10, 20);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                throw new InterruptedException("Sleep Failes :(");
+            }
+        }
+    }
+    public static void drawBackground(){
+        c.setInkColor(Color.black);
+        c.drawFilledRectangle(0, 0, 1000, 500);
+    }
+    public static void serpinskisTriangle(int i, int x1, int y1, int x2, int y2){
+        c.setInkColor(Color.cyan);
+        int x3 = x2+(x2-x1);
+        int y3 = y1;
+        c.drawLine(x1, y1, x2, y2);
+        c.drawLine(x2, y2, x3, y3);
+        c.drawLine(x3, y3, x1, y1);
+        if(i <= 0){
+            return;
+        }
+        int xmid1 = (x1+x2)/2;
+        int xmid2 = (x3+x2)/2;
+        int ymid = (y1+y2)/2;
+        serpinskisTriangle(i-1, x1, y1, xmid1, ymid);
+        serpinskisTriangle(i-1, xmid1, ymid, x2, y2);
+        serpinskisTriangle(i-1, x2, y1, xmid2, ymid);
+    }
+}
--- AP_CompSci/src/chapter11/baseFromPowerRecursive.java
+++ AP_CompSci/src/chapter11/baseFromPowerRecursive.java
@@ -2,7 +2,7 @@ package chapter11;
 
 import java.util.Scanner;
 
-public class baseFromPowerRecursive{
+public class BaseFromPowerRecursive{
     public static void main (String [] args){
         @SuppressWarnings("resource")
         Scanner in = new Scanner(System.in);
@@ -12,7 +12,10 @@ public class baseFromPowerRecursive{
         int base = in.nextInt();
         System.out.println("Base: " + calcBase(pow, base));
     }
-    public static int calcBase(int pow, int base){
-        return 0;
+    public static String calcBase(int pow, int base){
+        if (pow == 1){
+            return base + "";
+        }
+        return base + "*" + calcBase(pow-1, base);
     }
 }

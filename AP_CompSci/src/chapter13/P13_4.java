7f1db488a09673568d17af5dd0356deba21d0537
Philip Lewander <swedishassassin1@outlook.com>
2020-01-22 10:11:53 -0600

    Update P13_4.java

--- AP_CompSci/src/chapter13/P13_4.java
+++ AP_CompSci/src/chapter13/P13_4.java
@@ -4,16 +4,14 @@ public class P13_4{
     public static void main (String [] args){
         String toSearch = "This is a sentance.";
         String toFind = "is ";
-        System.out.println(search(toSearch, toFind, toSearch.length()));
+        System.out.println(search(toSearch, toFind, toSearch.length()-1));
         
     }
     public static boolean search(String toSearch, String toFind, int i){
-        if(i == 0){
-            return false;
+        if (toSearch.substring(0,toFind.length()-1).equals(toFind)){
+            return true;
+        } else {
+            return search(toSearch.substring(1), toFind, i - 1);
         }
-        if (toSearch.charAt(i) == toFind.charAt(0)){
-            search(toSearch.subString(1,toSearch.length()-1), toFind.subString(1,toFind.length()-1), i);
-        }
-        return search(toSearch.subString(1,toSearch.length()-1), toFind, i-1);
     }   
 }

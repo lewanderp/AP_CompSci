3b51bcbe31c3291228d578a9914d9675eadcb08b
Philip Lewander <swedishassassin1@outlook.com>
2020-02-12 11:20:21 -0600

    Added files from Mr.Due, fixed formatting and supressed warnings

---
+++ AP_CompSci/src/chapter14/filesFromDue/binsearch/BinarySearchDemo.java
@@ -0,0 +1,32 @@
+package chapter14.filesFromDue.binsearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * This program demonstrates the binary search algorithm.
+ */
+public class BinarySearchDemo {
+   public static void main(String[] args) {
+      // Construct random array
+
+      int[] a = ArrayUtil.randomIntArray(20, 100);
+      Arrays.sort(a);
+      System.out.println(Arrays.toString(a));
+      BinarySearcher searcher = new BinarySearcher(a);
+      @SuppressWarnings("resource")
+      Scanner in = new Scanner(System.in);
+
+      boolean done = false;
+      while (!done) {
+         System.out.print("Enter number to search for, -1 to quit:");
+         int n = in.nextInt();
+         if (n == -1)
+            done = true;
+         else {
+            int pos = searcher.search(n);
+            System.out.println("Found in position " + pos);
+         }
+      }
+   }
+}

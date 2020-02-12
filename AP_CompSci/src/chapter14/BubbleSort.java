05a97328402e2cdc19362cc9ecc31875d1b3ef6f
Philip Lewander <swedishassassin1@outlook.com>
2020-02-12 10:54:55 -0600

    formatting

--- AP_CompSci/src/chapter14/BubbleSort.java
+++ AP_CompSci/src/chapter14/BubbleSort.java
@@ -1,36 +1,37 @@
 package chapter14;
 
-public class BubbleSort{
-    private static int [] a;
-    public BubbleSort (int [] b){
+public class BubbleSort {
+    private static int[] a;
+
+    public BubbleSort(int[] b) {
         initA(b);
     }
 
-    private void initA(int[] b){
+    private void initA(int[] b) {
         a = b.clone();
     }
 
-    public int[] getASorted(){
+    public int[] getASorted() {
         return bubbleSort();
     }
 
-    public String printA(){
+    public String printA() {
         String s = "";
-        s+="[ ";
-        for(int i =0; i< a.length-2; i++) {
-            s+=a[i]+", ";
+        s += "[ ";
+        for (int i = 0; i < a.length - 2; i++) {
+            s += a[i] + ", ";
         }
-        s+=a[a.length-1]+" ]";
+        s += a[a.length - 1] + " ]";
         return s;
     }
 
-    private int[] bubbleSort(){
+    private int[] bubbleSort() {
 
         boolean isSorted = false;
 
-        for(int i =0; i< a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             isSorted = true;
-            for (int j = 1; j< a.length; j++){
+            for (int j = 1; j < a.length; j++) {
                 if (a[j - 1] > a[j]) {
                     int temp = a[j - 1];
                     a[j - 1] = a[j];
@@ -38,7 +39,7 @@ public class BubbleSort{
                     isSorted = false;
                 }
             }
-            if(isSorted){
+            if (isSorted) {
                 return a;
             }
         }
--- AP_CompSci/src/chapter14/InstertSort.java
+++ AP_CompSci/src/chapter14/InstertSort.java
@@ -1,41 +1,41 @@
 package chapter14;
 
-public class InstertSort{
-    
+public class InstertSort {
+
     private int[] a;
 
-    public InstertSort(int[] b){
+    public InstertSort(int[] b) {
         AInit(b);
     }
 
-    public int[] getASorted(){
+    public int[] getASorted() {
         return insertSort(a);
     }
 
-    public String printA(){
+    public String printA() {
         String s = "";
-        s+="[ ";
-        for(int i =0; i< a.length-2; i++) {
-            s+=a[i]+", ";
+        s += "[ ";
+        for (int i = 0; i < a.length - 2; i++) {
+            s += a[i] + ", ";
         }
-        s+=a[a.length-1]+" ]";
+        s += a[a.length - 1] + " ]";
         return s;
     }
 
-    private void AInit(int [] b){
+    private void AInit(int[] b) {
         a = b.clone();
     }
 
-    private int[] insertSort(int [] b){
-        int n = b.length; 
-        for (int i = 1; i < n; ++i) { 
-            int key = b[i]; 
-            int j = i - 1; 
-            while (j >= 0 && b[j] > key) { 
-                b[j + 1] = b[j]; 
-                j = j - 1; 
-            } 
-            b[j + 1] = key; 
+    private int[] insertSort(int[] b) {
+        int n = b.length;
+        for (int i = 1; i < n; ++i) {
+            int key = b[i];
+            int j = i - 1;
+            while (j >= 0 && b[j] > key) {
+                b[j + 1] = b[j];
+                j = j - 1;
+            }
+            b[j + 1] = key;
         }
         a = b.clone();
         return b;
